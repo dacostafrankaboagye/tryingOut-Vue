96ecdd877643eb71fd427ea69f960ed93f3e7885
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2023-02-17 02:57:42 +0000

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,57 @@
+
+<script>
+  export default {
+    data(){
+      return {
+        fullname:"Frank Aboagye",
+        clsname : "theFullname",
+        count: 0
+      }
+    },
+    methods:{
+      incre(){
+        this.count++
+      },
+      decre(){
+        this.count--
+      }
+    }
+  }
+</script>
+
+<template>
+  
+  <h1 v-bind:class="clsname">
+    Full name  = {{fullname}}
+  </h1>
+  
+  <br>
+  
+  <button v-on:click="incre">
+    Increment
+  </button>
+  
+    <br>
+  
+  <button v-on:click="decre">
+    Decrement
+  </button>
+  
+    <br>
+  
+  <h2>
+    The count is {{count}}
+  </h2>
+  
+  
+
+</template>
+
+
+<style>
+  
+  .theFullname{
+    color: blue;
+  }
+
+</style>
